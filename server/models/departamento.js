aa4d70d568648080e5d0a59b51dbcb7e3de64c79
Raul Emmanuel Garcia Delgadillo <190464@utags.edu.mx>
2020-12-16 23:56:32 -0600

    cracion de la coleccion departamentos

---
+++ server/models/departamento.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let departamentoSchema = new Schema({
+
+    id_jefe_de_area: {
+        type: ObjectId,
+        required: true
+  },
+
+  nombre: {
+    type: String,
+    required: true
+},
+
+numero_empleados: {
+    type: Number,
+    required: true
+},
+
+extension_telefonica: {
+    type: Number,
+    required: true
+},
+
+activo: {
+    type: Boolean,
+    default: true,
+    required: true
+},
+
+});
+
+module.exports = mongoose.model('departamento', departamentoSchema);
